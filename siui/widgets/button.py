db4288e96db3d28b4363250a495cf2656d6e1e73
IceF <1489423523@qq.com>
2024-07-04 22:08:34 +0800

    优化按钮的继承结构

--- siui/widgets/button.py
+++ siui/widgets/button.py
@@ -1,45 +1,35 @@
-from siui.widgets.abstracts import ABCPushButton, ABCAnimatedWidget
-from siui.widgets.label import SiLabel
-from siui.gui.colorsets import colorset
-from siui.gui import SiFont
-
 from PyQt5.QtCore import Qt
 
-class SiPushButtonBlank(ABCPushButton):
-    """
-    所有类型的SiPushButton的基类，只有按钮图形和按钮功能，没有文字或图标\n
-    但它提供一个可供绑定的接口，用于绑定任何将放置在按钮正中央的部件
-    """
+from siui.gui import SiFont
+from siui.gui.colorsets import colorset
+from siui.widgets.abstracts import ABCAnimatedWidget, ABCPushButton
+from siui.widgets.label import SiIconLabel, SiLabel
 
+
+class SiPushButton(ABCPushButton):
+    """
+    PushButton
+    """
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.themed = False  # 是否使用主题颜色
 
-        # 占位用的被绑定部件，显示在按钮正中央
-        self.attachment = ABCAnimatedWidget()
+        # 实例化文本标签
+        self.label = SiIconLabel(self)
+        self.label.setAutoAdjustSize(True)
+        self.label.setFont(SiFont.fromToken("S_BOLD"))
+        self.label.setStyleSheet(f"color: {colorset.color.TEXT_GRAD_HEX[1]}")
+        self.label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
 
-        # 按钮表面
-        self.body_top = SiLabel(self)
-        self.body_top.lower()
-
-        # 绘制最底层阴影部分
-        self.body_bottom = SiLabel(self)
-        self.body_bottom.lower()
+        # 绑定到主体
+        self.setAttachment(self.label)
 
     def reloadStylesheet(self):
         super().reloadStylesheet()
 
-        # 设置按钮表面的圆角边框
-        self.body_top.setFixedStyleSheet("""
-            border-top-left-radius: 4px;
-            border-top-right-radius: 4px;
-            border-bottom-left-radius: 2px;
-            border-bottom-right-radius: 2px;
-        """)
-
-        # 设置按钮阴影的圆角边框
-        self.body_bottom.setFixedStyleSheet("border-radius: 4px")
+        # 设置文字颜色
+        self.label.setStyleSheet(f"color: {colorset.color.TEXT_GRAD_HEX[0]}")
 
         # 设置按钮表面和阴影的颜色
         if self.themed is True:
@@ -56,61 +46,34 @@ class SiPushButtonBlank(ABCPushButton):
             self.body_top.setStyleSheet(f"background-color: {colorset.color.BTN_NORM_HEX[0]}")
             self.body_bottom.setStyleSheet(f"background-color: {colorset.color.BTN_NORM_HEX[1]}")
 
-    def setAttachment(self, widget):
+    def setThemed(self, b: bool):
         """
-        设置绑定部件。被绑定部件将会被设为按钮的子控件，并显示在按钮的正中央
-        :param widget: 部件
+        设置按钮是否成为主题按钮
+        :param b: 是否设为主题按钮
         :return:
         """
-        self.attachment = widget
-        self.attachment.setParent(self)
-        self.resize(self.size())  # 实现刷新位置
+        self.themed = b
 
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
-        size = event.size()
-        w, h = size.width(), size.height()
-
-        self.body_top.resize(w, h - 3)
-        self.body_bottom.resize(w, h)
-
-        self.attachment.move((w - self.attachment.width()) // 2, (h - 3 - self.attachment.height()) // 2)
-
-
-class SiPushButton(SiPushButtonBlank):
-    """
-    可以设置按钮文字的 PushButton
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # 实例化文本标签
-        self.text_label = SiLabel(self)
-        self.text_label.setAutoAdjustSize(True)
-        self.text_label.setFont(SiFont.fromToken("S_BOLD"))
-        self.text_label.setStyleSheet(f"color: {colorset.color.TEXT_GRAD_HEX[1]}")
-        self.text_label.setAlignment(Qt.AlignVCenter | Qt.AlignHCenter)
-
-        # 绑定到主体
-        self.setAttachment(self.text_label)
-
-    def setText(self, text):
+    def load(self, path_or_data):
         """
-        设置按钮的文本
-        :param text: 文本
+        从字符串或者文件加载 svg 数据
+        :param path_or_data: 文件路径或是 svg 字符串
         :return:
         """
-        self.text_label.setText(text)
+        self.label.load(path_or_data)
+
+    def setSvgSize(self, w, h):
+        """
+        设置 svg 图标的大小
+        :param w: 宽度
+        :param h: 高度
+        """
+        self.label.setSvgSize(w, h)
+
+    def setText(self, text: str):
+        self.label.setText(text)
 
     def text(self):
-        """
-        获取按钮的文本
-        :return:
-        """
-        return self.text_label.text()
+        return self.label.text()
 
-    def reloadStylesheet(self):
-        super().reloadStylesheet()
-
-        self.text_label.setStyleSheet(f"color: {colorset.TEXT_GRAD_HEX[0]}")
 
--- siui/widgets/label.py
+++ siui/widgets/label.py
@@ -1,5 +1,6 @@
 from PyQt5.QtCore import QPoint, Qt, pyqtSignal
 from PyQt5.QtGui import QPainter, QPainterPath, QPixmap
+from PyQt5.QtSvg import QSvgWidget
 
 from siui.core.animation import SiExpAnimation
 from siui.core.color import Color
@@ -111,6 +112,120 @@ class SiPixLabel(SiLabel):
     def resizeEvent(self, event):
         super().resizeEvent(event)
         self.draw()
+
+
+class SiSvgLabel(SiLabel):
+    """
+    可以显示 Svg 图像的 SiLabel 标签
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 创建 QSvgWidget
+        self.svg_widget = QSvgWidget(self)
+
+    def load(self, path_or_data):
+        """
+        从字符串或者文件加载 svg 数据
+        :param path_or_data: 文件路径或是 svg 字符串
+        :return:
+        """
+        self.svg_widget.load(path_or_data)
+
+    def setSvgSize(self, w, h):
+        """
+        设置 svg 图标的大小
+        :param w: 宽度
+        :param h: 高度
+        """
+        self.svg_widget.setFixedSize(w, h)
+        self.resize(self.size())    # 保证居中
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        size = event.size()
+        w, h = size.width(), size.height()
+
+        # 保证居中显示
+        self.svg_widget.move((w - self.svg_widget.width()) // 2, (h - self.svg_widget.height()) // 2)
+
+
+class SiIconLabel(SiLabel):
+    """
+    具图标的标签，即一个图标紧跟一段文字的标签，使用一个 SiSvgLabel 和 SiLabel 组合
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.has_icon_flag = False  # 加载图标后为真
+        self.has_text_flag = False  # 设置文本后为真
+
+        # 创建图标标签
+        self.icon = SiSvgLabel(self)
+        self.icon.resize(16, 16)
+        self.icon.setSvgSize(16, 16)
+
+        # 创建文本标签
+        self.text_label = SiLabel(self)
+        self.text_label.setAutoAdjustSize(True)
+        self.text_label.setAlignment(Qt.AlignVCenter | Qt.AlignLeft)
+
+    def setStyleSheet(self, stylesheet: str):
+        self.text_label.setStyleSheet(stylesheet)
+
+    def setFixedStyleSheet(self, fixed_stylesheet: str):
+        self.text_label.setStyleSheet(fixed_stylesheet)
+
+    def load(self, path_or_data):
+        """
+        从字符串或者文件加载 svg 数据
+        :param path_or_data: 文件路径或是 svg 字符串
+        :return:
+        """
+        self.has_icon_flag = True
+        self.icon.load(path_or_data)
+        self.adjustSize()    # 保证布局正常
+
+    def setSvgSize(self, w, h):
+        """
+        设置 svg 图标的大小
+        :param w: 宽度
+        :param h: 高度
+        """
+        self.icon.resize(w, h)  # 这里直接设为一样，避免边缘切割
+        self.icon.setSvgSize(w, h)
+        self.adjustSize()    # 保证布局正常
+
+    def setFont(self, a0):
+        self.text_label.setFont(a0)
+
+    def setText(self, text: str):
+        self.has_text_flag = True
+        self.text_label.setText(text)
+        self.adjustSize()    # 保证布局正常
+
+    def text(self):
+        return self.text_label.text()
+
+    def adjustSize(self):
+
+
+        preferred_width = (int(self.has_text_flag) * self.text_label.width() +
+                           int(self.has_text_flag and self.has_icon_flag) * 4 +
+                           int(self.has_icon_flag) * self.icon.width())
+
+        preferred_height = max(int(self.has_text_flag) * self.text_label.height(),
+                               int(self.has_icon_flag) * self.icon.height())
+
+        self.resize(preferred_width, preferred_height)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        size = event.size()
+        w, h = size.width(), size.height()
+        self.icon.move(0, (h - self.icon.height()) // 2)
+        self.text_label.move(w - self.text_label.width(), (h - self.text_label.height()) // 2)
+
 
 
 class SiDraggableLabel(SiLabel):
