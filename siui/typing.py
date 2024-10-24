344b2cfd332a3f79ee0c10e365245d6bd3f4f92b
rainzee <rainzee.w@gmail.com>
2024-10-25 00:31:17 +0800

    feat: impl `typing` module

---
+++ siui/typing.py
@@ -0,0 +1,10 @@
+"""
+## This module defines global shared types
+
+Use Python's Type Hint syntax, reference:
+- [`typing`](https://docs.python.org/3/library/typing.html)
+- [`PEP 484`](https://www.python.org/dev/peps/pep-0484/)
+- [`PEP 526`](https://www.python.org/dev/peps/pep-0526/)
+"""
+
+from typing_extensions import TypeAlias
