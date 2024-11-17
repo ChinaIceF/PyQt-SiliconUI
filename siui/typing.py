46dd2c1bac73b766784de0cb2de0e4b29321c166
rainzee <rainzee.w@gmail.com>
2024-11-17 17:59:59 +0800

    feat(typing): add `T_WidgetParent` and `T_ObjectParent`

--- siui/typing.py
+++ siui/typing.py
@@ -7,4 +7,14 @@ Use Python's Type Hint syntax, reference:
 - [`PEP 526`](https://www.python.org/dev/peps/pep-0526/)
 """
 
+from typing import Optional
+
+from PyQt5.QtCore import QObject
+from PyQt5.QtWidgets import QWidget
 from typing_extensions import TypeAlias
+
+T_WidgetParent: TypeAlias = Optional[QWidget]
+"""Type of widget parent"""
+
+T_ObjectParent: TypeAlias = Optional[QObject]
+"""Type of object parent"""
