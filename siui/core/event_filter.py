432b88861a2a1c89387de70665e197c56bdf442d
ChinaIceF <1489423523@qq.com>
2025-07-02 17:30:08 +0800

    feat: impl `ButtonScaleOnPressedManager`

--- siui/core/event_filter.py
+++ siui/core/event_filter.py
@@ -5,6 +5,10 @@ from siui.components.tooltip import ToolTipWindow
 
 
 class TooltipManager(QObject):
+    """
+    管理工具提示的管理器
+    忽略原版工具提示，并把工具提示发送到自定义工具提示窗口上，提供操作工具提示窗口的接口
+    """
     def __init__(self, tooltip_window: ToolTipWindow):
         super().__init__()
         self._tooltip_window = tooltip_window
@@ -45,5 +49,46 @@ class TooltipManager(QObject):
 
         elif event.type() == QEvent.ToolTip:
             return True
+
+        return False
+
+
+class ButtonScaleOnPressedManager(QObject):
+    """
+    管理 `scaleFactor` 属性的 `SiExpAnimation` 的管理器
+    实现鼠标按下控件时产生缩放效果，需要父控件具有 `animation` 方法，且具有 `scaleFactor` 属性
+    """
+    def __init__(self, parent: QWidget):
+        super().__init__(parent)
+
+        self._max_scale_factor = 1.0
+        self._min_scale_factor = 0.9
+
+    def setMaxScaleFactor(self, value: float) -> None:
+        self._max_scale_factor = value
+
+    def setMinScaleFactor(self, value: float) -> None:
+        self._min_scale_factor = value
+
+    def maxScaleFactor(self) -> float:
+        return self._max_scale_factor
+
+    def minScaleFactor(self) -> float:
+        return self._min_scale_factor
+
+    def eventFilter(self, obj: QWidget, event):
+        if event.type() == QEvent.MouseButtonPress or event.type() == QEvent.MouseButtonDblClick:
+            ani = self.parent().animation("scaleFactor")
+            ani.setFactor(1/16)
+            ani.setBias(0)
+            ani.setEndValue(self._min_scale_factor)
+            ani.start()
+
+        elif event.type() == QEvent.MouseButtonRelease:
+            ani = self.parent().animation("scaleFactor")
+            ani.setFactor(1/4)
+            ani.setBias(0.001)
+            ani.setEndValue(self._max_scale_factor)
+            ani.start()
 
         return False
